8121baef8f45191cd743fe8d9995ea3d53ceb2b4
Nicholas Young <ndy14@ufl.edu>
2019-08-02 00:25:44 -0400

    Initial commit, skeleton code

---
+++ src/main/java/com/yungnickyoung/minecraft/bettercaves/BetterCaves.java
@@ -0,0 +1,13 @@
+package com.yungnickyoung.minecraft.bettercaves;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = BetterCaves.MODID, name = BetterCaves.NAME, version = BetterCaves.VERSION, useMetadata = true)
+public class BetterCaves {
+    // MOD INFORMATION CONSTANTS
+    // These should not actually be used since 'useMetaData' is true.
+    // See resources/mcmod.info for actual used information.
+    public static final String MODID = "bettercaves";
+    public static final String NAME = "Better Caves";
+    public static final String VERSION = "0.0.1";
+}

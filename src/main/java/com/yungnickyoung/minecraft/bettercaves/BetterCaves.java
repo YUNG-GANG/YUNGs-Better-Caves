df9a106624afd2d723b13159c2d72279fef1f528
Nicholas Young <ndy14@ufl.edu>
2019-08-02 02:11:45 -0400

    Remove mod dependencies due to issues. Got logger working and added basic log command to entrypoint class. Change version of Forge to latest instead of last stable

--- src/main/java/com/yungnickyoung/minecraft/bettercaves/BetterCaves.java
+++ src/main/java/com/yungnickyoung/minecraft/bettercaves/BetterCaves.java
@@ -1,7 +1,12 @@
 package com.yungnickyoung.minecraft.bettercaves;
 
+import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
-
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 @Mod(modid = BetterCaves.MODID, name = BetterCaves.NAME, version = BetterCaves.VERSION, useMetadata = true)
 public class BetterCaves {
     // MOD INFORMATION CONSTANTS
@@ -10,4 +15,18 @@ public class BetterCaves {
     public static final String MODID = "bettercaves";
     public static final String NAME = "Better Caves";
     public static final String VERSION = "0.0.1";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = LogManager.getLogger();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
 }

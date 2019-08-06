836fe8e477eef77080045f0b64c53c43e00705e3
Nicholas Young <ndy14@ufl.edu>
2019-08-05 20:17:05 -0400

    ridged multi-fractal functions using FastNoise

--- src/main/java/com/yungnickyoung/minecraft/bettercaves/world/BetterCaveGenerator.java
+++ src/main/java/com/yungnickyoung/minecraft/bettercaves/world/BetterCaveGenerator.java
@@ -2,11 +2,13 @@ package com.yungnickyoung.minecraft.bettercaves.world;
 
 import com.yungnickyoung.minecraft.bettercaves.config.Configuration;
 import com.yungnickyoung.minecraft.bettercaves.config.Settings;
+import com.yungnickyoung.minecraft.bettercaves.util.FastNoise;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkPrimer;
 import net.minecraft.world.gen.MapGenCaves;
 
@@ -19,6 +21,16 @@ public class BetterCaveGenerator extends MapGenCaves {
     private int lavaDepth;
     private double FThreshold;
 
+    // DEBUG VALS
+    private int numChunksGenerated = 0;
+    private double avgNoise1 = 0;
+    private double avgNoise2 = 0;
+    private double maxNoise1 = -10;
+    private double minNoise1 = 10;
+    private double maxNoise2 = -10;
+    private double minNoise2 = 10;
+
+
     public BetterCaveGenerator() {
         this.lavaDepth = Configuration.cavegen.lavaDepth;
         this.FThreshold = Configuration.cavegen.FThreshold;
@@ -28,9 +40,93 @@ public class BetterCaveGenerator extends MapGenCaves {
     @ParametersAreNonnullByDefault
     public void generate(World worldIn, int chunkX, int chunkZ, ChunkPrimer primer) {
 //        Settings.LOGGER.info("generate() cave called: {} | {} | {} | {}", worldIn, chunkX, chunkZ, primer);
-
         this.world = worldIn;
 
+//        generateWorleys(chunkX, chunkZ, primer);
+        generateFractal(chunkX, chunkZ, primer);
+    }
+
+
+
+
+    private void generateFractal(int chunkX, int chunkZ, ChunkPrimer primer) {
+        FastNoise noiseGenerator1 = new FastNoise(primer.hashCode());
+        noiseGenerator1.SetNoiseType(FastNoise.NoiseType.PerlinFractal);
+        noiseGenerator1.SetFractalType(FastNoise.FractalType.RigidMulti); // TODO - try rigid multi?
+
+        FastNoise noiseGenerator2 = new FastNoise(primer.hashCode() + 1);
+        noiseGenerator2.SetNoiseType(FastNoise.NoiseType.PerlinFractal);
+        noiseGenerator2.SetFractalType(FastNoise.FractalType.RigidMulti); // TODO - try rigid multi?
+
+
+//        noiseGenerator2.SetFractalOctaves(1);
+//        noiseGenerator2.SetFrequency(2);
+//        noiseGenerator.SetFractalLacunarity();
+        noiseGenerator1.SetFractalGain(.2f);
+        noiseGenerator2.SetFractalGain(.2f);
+        noiseGenerator1.SetFrequency(.05f);
+
+        for (int localX = 0; localX < 16; localX++) {
+            int realX = localX + chunkX;
+
+            for (int localZ = 0; localZ < 16; localZ++) {
+                int realZ = localZ + chunkZ;
+
+                for (int realY = 64; realY > 0; realY--) {
+                    float noise1 = noiseGenerator1.GetNoise(realX, realY, realZ);
+                    float noise2 = noiseGenerator2.GetNoise(realX, realY, realZ);
+
+//                    Settings.LOGGER.info("noise1: {}, noise2: {}", noise1, noise2);
+
+                    int state1 = (noise1 < .7) ? 1 : 0;
+                    int state2 = (noise2 < .7) ? 1 : 0;
+                    int state = state1 * state2;
+
+                    if (state == 1) {
+                        IBlockState currentBlockState = primer.getBlockState(localX, realY, localZ);
+                        IBlockState aboveBlockState = primer.getBlockState(localX, realY + 1, localZ);
+                        boolean foundTopBlock = isTopBlock(primer, localX, realY, localZ, chunkX, chunkZ);
+                        digBlock(primer, localX, realY, localZ, chunkX, chunkZ, foundTopBlock, currentBlockState, aboveBlockState);
+                    }
+
+                    avgNoise1 = ((numChunksGenerated * avgNoise1) + noise1) / (numChunksGenerated + 1);
+                    avgNoise2 = ((numChunksGenerated * avgNoise2) + noise2) / (numChunksGenerated + 1);
+
+                    if (noise1 > maxNoise1) maxNoise1 = noise1;
+                    if (noise2 > maxNoise2) maxNoise2 = noise2;
+                    if (noise1 < minNoise1) minNoise1 = noise1;
+                    if (noise2 < minNoise2) minNoise2 = noise2;
+
+                    numChunksGenerated++;
+
+                    if (numChunksGenerated == 2000) {
+                        Settings.LOGGER.info("300 Chunks Generated Report");
+
+                        Settings.LOGGER.info("--> Noise 1");
+                        Settings.LOGGER.info("  > Average: {}", avgNoise1);
+                        Settings.LOGGER.info("  > Max: {}", maxNoise1);
+                        Settings.LOGGER.info("  > Min: {}", minNoise1);
+
+                        Settings.LOGGER.info("--> Noise 2");
+                        Settings.LOGGER.info("  > Average: {}", avgNoise2);
+                        Settings.LOGGER.info("  > Max: {}", maxNoise2);
+                        Settings.LOGGER.info("  > Min: {}", minNoise2);
+
+                        // Reset vals
+                        numChunksGenerated = 0;
+                        avgNoise1 = 0;
+                        avgNoise2 = 0;
+                        maxNoise1 = -10;
+                        minNoise1 = 10;
+                        maxNoise2 = -10;
+                        minNoise2 = 10;
+                    }
+                }
+            }
+        }
+    }
+
+    private void generateWorleys(int chunkX, int chunkZ, ChunkPrimer primer) {
         Random r = new Random(primer.hashCode()); // seed rng with this chunk
         ArrayList<Float3> featurePointList = new ArrayList<>();
 
@@ -67,11 +163,7 @@ public class BetterCaveGenerator extends MapGenCaves {
                         }
                     }
 
-                    double F1Threshold = 15;
-                    double F2Threshold = 5.0;
-
                     double F = Math.abs(F2 - F1);
-
 
                     if (F < FThreshold) {
                         IBlockState currentBlockState = primer.getBlockState(localX, localY, localZ);
@@ -82,17 +174,6 @@ public class BetterCaveGenerator extends MapGenCaves {
                 }
             }
         }
-
-//        for (int localX = 0; localX < 16; localX++) {
-//            for (int localZ = 0; localZ < 16; localZ++) {
-//                for (int localY = 0; localY < 80; localY++) {
-//                    IBlockState currentBlockState = primer.getBlockState(localX, localY, localZ);
-//                    IBlockState aboveBlockState = primer.getBlockState(localX, localY + 1, localZ);
-//                    boolean foundTopBlock = isTopBlock(primer, localX, localY, localZ, chunkX, chunkZ);
-//                    digBlock(primer, localX, localY, localZ, chunkX, chunkZ, foundTopBlock, currentBlockState, aboveBlockState);
-//                }
-//            }
-//        }
     }
 
     private double distance(Float3 point1, Float3 point2) {

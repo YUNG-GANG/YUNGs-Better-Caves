e4b3a9d82b12328521f5b2de4460c4c4daa8a612
Nicholas Young <ndy14@ufl.edu>
2019-10-04 00:21:40 -0400

    Update config comment for debug visualizer

--- src/main/java/com/yungnickyoung/minecraft/bettercaves/config/ConfigDebug.java
+++ src/main/java/com/yungnickyoung/minecraft/bettercaves/config/ConfigDebug.java
@@ -7,9 +7,10 @@ public class ConfigDebug {
     @Config.Comment("The visualizer creates worlds where there are no blocks except those indicating where caves" +
             " and caverns would be carved out in a regular world. This is useful for visualizing the kinds of" +
             "caves and caverns your current config options will create.\n" +
-            "Type 1 Cave: Quartz Block\n" +
+            "Type 1 Cave: Wooden Planks\n" +
             "Type 2 Cave: Cobblestone\n" +
             "Lava Cavern: Redstone Block\n" +
-            "Floored Cavern: Gold Block")
+            "Floored Cavern: Gold Block\n + " +
+            "Water Cavern: Lapis Block")
     public boolean debugVisualizer = false;
 }

2fcba392d4f2b9707ec0334a7f72600e57396246
Nicholas Young <ndy14@ufl.edu>
2019-11-30 22:10:15 -0500

    Remove parenthetical 'cubic' and 'simplex' details in config, and adjust config comments. Users don't really need to be exposed to the words 'cubic' and 'simplex' as it only serves as a point of confusion

--- src/main/java/com/yungnickyoung/minecraft/bettercaves/config/cave/ConfigCaves.java
+++ src/main/java/com/yungnickyoung/minecraft/bettercaves/config/cave/ConfigCaves.java
@@ -4,14 +4,16 @@ import com.yungnickyoung.minecraft.bettercaves.enums.CaveBiomeSize;
 import net.minecraftforge.common.config.Config;
 
 public class ConfigCaves {
-    @Config.Name("Type 1 Caves (Cubic)")
-    @Config.Comment("Parameters used in the generation of caves generated with cubic noise. Does not include the" +
-            " large caverns found at low altitudes.")
+    @Config.Name("Type 1 Caves")
+    @Config.Comment("Parameters used in the generation of caves made with cubic noise. Type 1 Caves are a little " +
+            "less spacious than Type 2 Caves, and have more long, winding tunnels instead of large rooms. Does not " +
+            "affect caverns found at low altitudes.")
     public ConfigCubicCave cubicCave = new ConfigCubicCave();
 
-    @Config.Name("Type 2 Caves (Simplex)")
-    @Config.Comment("Parameters used in the generation of caves generated with simplex noise. Does not include the" +
-            " large caverns found at low altitudes.")
+    @Config.Name("Type 2 Caves")
+    @Config.Comment("Parameters used in the generation of caves made with simplex noise. Type 2 Caves tend to have " +
+            "more open, spacious rooms than Type 1 Caves, with shorter winding passages. Does not include the " +
+            "large caverns found at low altitudes.")
     public ConfigSimplexCave simplexCave = new ConfigSimplexCave();
 
     @Config.Name("Vanilla Caves")
@@ -21,7 +23,7 @@ public class ConfigCaves {
     @Config.Name("Cave Biome Size")
     @Config.Comment("Determines how large cave biomes are. Controls how long a cave system of a certain cave type" +
             " extends before intersecting with a cave system of another type. Larger Biome Size = more " +
-            "cave interconnectivity.")
+            "cave interconnectivity for a given area, but possibly less variation.")
     @Config.RequiresWorldRestart
     public CaveBiomeSize caveBiomeSize = CaveBiomeSize.Large;
 

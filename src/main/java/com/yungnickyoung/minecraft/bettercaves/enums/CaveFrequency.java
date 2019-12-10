5977a8bd09821d5b179c53fd3dcaa9b6957d9bc8
Nicholas Young <ndy14@ufl.edu>
2019-12-09 22:41:37 -0500

    Remove enums for sizes and frequencies. They are unused due to enums not being a supported type in Forge for 1.14

--- src/main/java/com/yungnickyoung/minecraft/bettercaves/enums/CaveFrequency.java
+++
@@ -1,8 +0,0 @@
-package com.yungnickyoung.minecraft.bettercaves.enums;
-
-/**
- * Enum used for spawn rates of different kinds of caves from Better Caves.
- */
-public enum CaveFrequency {
-    None, Rare, Common, VeryCommon
-}
--- src/main/java/com/yungnickyoung/minecraft/bettercaves/enums/WaterBiomeFrequency.java
+++
@@ -1,5 +0,0 @@
-package com.yungnickyoung.minecraft.bettercaves.enums;
-
-public enum WaterBiomeFrequency {
-    Rare, Normal, Common, VeryCommon, Always
-}

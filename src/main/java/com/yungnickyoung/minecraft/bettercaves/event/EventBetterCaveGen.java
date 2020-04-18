09d95dcac6fb4faeb777e8946dfdf32e079a2ff4
Nicholas Young <ndy14@ufl.edu>
2020-04-17 22:22:04 -0400

    Now only overrides vanilla mineshafts so other mods can take higher priority. Update mod description.

--- src/main/java/com/yungnickyoung/minecraft/bettercaves/event/EventBetterCaveGen.java
+++ src/main/java/com/yungnickyoung/minecraft/bettercaves/event/EventBetterCaveGen.java
@@ -14,19 +14,25 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class EventBetterCaveGen {
     /**
      * Replaces cave gen and mineshaft gen
+     *
      * @param event Map generation event
      */
     @SubscribeEvent(priority = EventPriority.NORMAL)
     public void onInitMapGenEvent(InitMapGenEvent event) {
         // Replace cave gen with Better Caves if the original gen passed isn't from Better Caves
-        if ((event.getType() == InitMapGenEvent.EventType.CAVE || event.getType() == InitMapGenEvent.EventType.NETHER_CAVE)
-                && !event.getOriginalGen().getClass().equals(MapGenBetterCaves.class)) {
+        if (
+            (event.getType() == InitMapGenEvent.EventType.CAVE || event.getType() == InitMapGenEvent.EventType.NETHER_CAVE)
+                && !event.getOriginalGen().getClass().equals(MapGenBetterCaves.class)
+        ) {
             Settings.LOGGER.debug("INITMAPGENEVENT");
             event.setNewGen(new MapGenBetterCaves(event));
         }
         // Replace mineshaft gen with Better Caves if the original gen passed in isn't from Better Caves
-        else if (event.getType() == InitMapGenEvent.EventType.MINESHAFT
-                && !event.getOriginalGen().getClass().equals(MapGenBetterMineshaft.class)) {
+        else if (
+            event.getType() == InitMapGenEvent.EventType.MINESHAFT
+                && !event.getOriginalGen().getClass().equals(MapGenBetterMineshaft.class)
+                && event.getOriginalGen() == event.getNewGen() // only modify vanilla gen to allow other mods to modify mineshafts
+        ) {
             event.setNewGen(new MapGenBetterMineshaft());
         }
     }

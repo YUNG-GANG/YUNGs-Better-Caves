57b9f70f7fe745283605fbd2ef812cccfb7e17f5
williambl <william@williambl.com>
2026-09-04 11:36:31 +0100

    Fix unsafe call to ServiceLoader#load
    See jaredlll08/MultiLoader-Template#97
    
    See YUNG-GANG/YUNGs-Better-Caves#198

--- Common/src/main/java/com/yungnickyoung/minecraft/bettercaves/services/Services.java
+++ Common/src/main/java/com/yungnickyoung/minecraft/bettercaves/services/Services.java
@@ -8,7 +8,7 @@ public class Services {
     public static final IPlatformHelper PLATFORM = load(IPlatformHelper.class);
 
     public static <T> T load(Class<T> clazz) {
-        final T loadedService = ServiceLoader.load(clazz)
+        final T loadedService = ServiceLoader.load(clazz, Services.class.getClassLoader())
                 .findFirst()
                 .orElseThrow(() -> new NullPointerException("Failed to load service for " + clazz.getName()));
         BetterCavesCommon.LOGGER.debug("Loaded {} for service {}", loadedService, clazz);
